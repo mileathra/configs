8b02ecf8300f74f51f3a3efc2e7f1b2852c97f49
mileathra <mileathra@gmail.com>
2025-12-19 09:35:14 +0300

    chore: add symbols for whitespace and tabs, add spellchecking

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -4,6 +4,7 @@ vim.o.relativenumber = true
 vim.o.tabstop = 4
 vim.o.shiftwidth = vim.o.tabstop
 
+vim.o.expandtab = true
 vim.o.smarttab = true
 
 vim.o.clipboard = "unnamedplus"
@@ -17,3 +18,11 @@ vim.o.signcolumn = "yes"
 vim.o.swapfile = false
 
 vim.o.ignorecase = true
+
+vim.opt.listchars:append("tab:⇒ ")
+vim.opt.listchars:append("space:·")
+vim.opt.listchars:append("trail:·")
+vim.o.list = true
+
+vim.o.spelllang = "en_us"
+vim.o.spell = true
